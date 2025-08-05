c08ce5e1154249de5941460be5e516c3a46196ee
Faiaz98 <iram.faiaz99@gmail.com>
2025-08-06 00:10:18 +0600

    chore: initial vite + tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-xl">
+      <p className="text-gray-500">🚧 Sortable List coming soon...</p>
+    </main>
+  );
+};
+
+export default App;
